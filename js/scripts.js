80753a6a5f9220ef7ae3e9138479da04ca4ee0bc
dylan crocker and Marissa Perry <dylan@>
2019-03-26 16:09:13 -0700

    have collabsible flashcards

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$().ready(function(){
+
+
+  $("#html").click(function(){
+    $(".collapsible-html").slideToggle();
+  });
+
+  $("#css").click(function(){
+    $(".collapsible-css").slideToggle();
+  });
+
+  $("#bs").click(function(){
+    $(".collapsible-bs").slideToggle();
+  });
+});
